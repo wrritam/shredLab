01e0869d6251733596fed11d0fe55294a6119b75
Ritam Samanta <bruceritamwayne02@gmail.com>
2024-04-03 11:30:53 +0530

    repo.ts work ongoing

--- backend/dist/controller/User/commit/repository.js
+++ backend/dist/controller/User/commit/repository.js
@@ -1,3 +1,27 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.createRepo = void 0;
+const db_config_1 = __importDefault(require("../../../db/db.config"));
+const createRepo = async (req, res) => {
+    const user = await db_config_1.default.user.findUnique({
+        where: { email: req.user.email },
+    });
+    if (user) {
+        const newRepo = await db_config_1.default.repository.create({
+            data: {
+                name: req.body.name,
+                readme: req.body.readme,
+                ownerId: parseInt(req.params.userid),
+            },
+        });
+        res.status(201).json({ message: "Repository created", data: newRepo.name });
+    }
+    else {
+        res.status(403).json({ message: "User not found" });
+    }
+};
+exports.createRepo = createRepo;
 //# sourceMappingURL=repository.js.map
--- backend/src/controller/User/commit/repository.ts
+++ backend/src/controller/User/commit/repository.ts
@@ -1,4 +1,30 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import prisma from "../../../db/db.config";
 
-export const createRepo = async (req: Request, res: Response) => {};
+interface User {
+  username: string;
+  email: string;
+  password: string;
+}
+interface UserRequest extends express.Request {
+  user: User;
+}
+
+export const createRepo = async (req: UserRequest, res: express.Response) => {
+  const user = await prisma.user.findUnique({
+    where: { email: req.user.email },
+  });
+
+  if (user) {
+    const newRepo = await prisma.repository.create({
+      data: {
+        name: req.body.name,
+        readme: req.body.readme,
+        ownerId: parseInt(req.params.userid),
+      },
+    });
+    res.status(201).json({ message: "Repository created", data: newRepo.name });
+  } else {
+    res.status(403).json({ message: "User not found" });
+  }
+};
